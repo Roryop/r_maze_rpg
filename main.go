67bb0b1bad78f8df572c49bb5b5d463be9a91d70
Roryop <roryop@tumo.world>
2024-10-19 10:53:18 +0200

    fixed matrix-size issue

--- main.go
+++ main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"fmt"
 	"start/enemies"
-	"start/random"
+	"start/rooms"
+	//"start/random"
 )
 
 func main() {
 	fmt.Println("hi")
-	//rooms.RandomMatrix("normal")
+	rooms.RandomMatrix("normal")
 
 	var ork1 = enemies.NewOrk()
 	fmt.Println(ork1.InitBasisOrk())
 	ork1.SetStats(10, 10, 10, 10)
 	ork1.SeeStats()
 
-	random.GetRandomNumber(100)
+	//random.GetRandomNumber(100)
 }
